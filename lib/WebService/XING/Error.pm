cf7127504421ebc630b96b5f50c3f8e80c00e4ae
Bernhard Graf <augensalat@gmail.com>
2012-03-16 20:15:23 +0100

    New attribute content_message.

--- lib/WebService/XING/Error.pm
+++ lib/WebService/XING/Error.pm
@@ -7,6 +7,9 @@ extends 'WebService::XING::Response';
 has error_name => (builder => '_build_error_name');
 sub _build_error_name { $_[0]->content->{error_name} }
 
+has content_message => (builder => '_build_content_message');
+sub _build_content_message { $_[0]->content->{message} }
+
 1;
 
 __END__
@@ -27,4 +30,10 @@ following:
 
 Error description string as returned by the XING API in an error response.
 Might be C<undef> if the API did not return an C<error_name> field in the
+response body.
+
+=head2 content_message
+
+Error message string as returned by the XING API in an error response.
+Might be C<undef> if the API did not return an C<message> field in the
 response body.
